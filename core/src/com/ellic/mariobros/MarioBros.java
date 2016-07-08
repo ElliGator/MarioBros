e4c7a65f4093499fe8c7d26a15dcb4629987d891
ElliGator <ecarranza@ufl.edu>
2016-07-08 16:59:49 -0400

    Reset to initial project build

--- core/src/com/ellic/mariobros/MarioBros.java
+++ core/src/com/ellic/mariobros/MarioBros.java
@@ -8,18 +8,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ellic.mariobros.Screens.PlayScreen;
 
-public class MarioBros extends Game {
+public class MarioBros extends ApplicationAdapter {
 	public SpriteBatch batch; //made public so that all screens have access to it
-
+	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this)); //allows game to set screens in the future
+		//setScreen(new PlayScreen(this)); //allows game to set screens in the future
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		super.render(); //delegates render method to active screen
+		//super.render(); //delegates render method to active screen
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//game.batch.setProjectionMatrix(gamecam.combined);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 }
--- core/src/com/ellic/mariobros/Screens/PlayScreen.java
+++ core/src/com/ellic/mariobros/Screens/PlayScreen.java
@@ -4,7 +4,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ellic.mariobros.MarioBros;
 
 /**
@@ -13,10 +16,15 @@ import com.ellic.mariobros.MarioBros;
 public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
+    //camera that follows game and actually shows what viewport is going to display
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gamecam = new OrthographicCamera();
+        gamePort = new StretchViewport(800, 480, gamecam);
     }
 
     @Override
@@ -29,6 +37,7 @@ public class PlayScreen implements Screen {
         //clears the screen
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -36,7 +45,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        //gamePort.update(width, height);
     }
 
     @Override
