dcab86deee27d7553c7c833a33439ecbe03a5bbb
ElliGator <ecarranza@ufl.edu>
2016-07-08 18:04:02 -0400

    Implemented viewport to adjust for different devices

--- core/src/com/ellic/mariobros/Screens/PlayScreen.java
+++ core/src/com/ellic/mariobros/Screens/PlayScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ellic.mariobros.MarioBros;
@@ -17,14 +19,20 @@ public class PlayScreen implements Screen {
     private MarioBros game;
     Texture texture;
     //camera that follows game and actually shows what viewport is going to display
-    //private OrthographicCamera gamecam;
-    //private Viewport gamePort;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
-        //gamecam = new OrthographicCamera();
-        //gamePort = new StretchViewport(800, 480, gamecam);
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(800, 480, gamecam);
+        /* stretch viewport streaches the graphics and can alter the way graphics look because
+         * aspect ratio is not kept
+         *
+         * screen viewport keeps aspect ratio but in bigger devices may reveal more of the game
+         * world and give these players an unfair advantage
+         */
     }
 
     @Override
@@ -37,7 +45,8 @@ public class PlayScreen implements Screen {
         //clears the screen
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //game.batch.setProjectionMatrix(gamecam.combined);
+        //where camera is in the game and show only what we want camera to show
+        game.batch.setProjectionMatrix(gamecam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -45,7 +54,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        //gamePort.update(width, height);
+        gamePort.update(width, height);
     }
 
     @Override
